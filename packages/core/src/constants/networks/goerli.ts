fc6b016ee68945b22e7274fec3628e728d7df535
Razvan Gabriel <razvan.gabriel.apostu@gmail.com>
2022-12-24 19:41:08 +0200

    refactor(core): move generated files into the same folder

--- packages/core/src/constants/networks/goerli.ts
+++ packages/core/src/constants/networks/goerli.ts
@@ -1,9 +1,13 @@
 export let network = "goerli";
+export let startBlock = 8122650;
 
 export let linear = ["0xb0Ed8e11339F1d655a3029a1e3d7e448326556c6"];
 export let periphery = [""];
 export let pro = ["0x3F1EB5f4426a6e1563F30532dDd16cc73C804529"];
 
-export let initializer = linear[0];
+/**
+ * The initializer contract is used to trigger the indexing of all other contracts.
+ * It should be a LINEAR contract, the oldest/first one deployed on this chain.
+ */
 
-export let startBlock = 8122650;
+export let initializer = linear[0];
---
+++ packages/core/src/mappings/action.ts
@@ -0,0 +1,6 @@
+// import { Vault as VaultTemplate } from "";
+// import { Vault } from "../../generated/schema";
+// import { getVaultIds } from "../constants";
+// import { Address, ethereum, log } from "@graphprotocol/graph-ts";
+
+function handleCreateLinear() {}
