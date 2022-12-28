a1c1727c35afa9650b23e8cbcea4da01229d60da
Razvan Gabriel <razvan.gabriel.apostu@gmail.com>
2022-12-28 02:11:09 +0200

    fix(core): manual handler for first stream not necessary

--- packages/core/src/mappings/initializer.ts
+++ packages/core/src/mappings/initializer.ts
@@ -3,7 +3,6 @@ import { ContractLinear, ContractPro } from "../generated/types/templates";
 import { CreateLinearStream } from "../generated/types/templates/ContractLinear/SablierV2Linear";
 import { getContractsLinear, getContractsPro } from "../constants";
 import { createContract, getOrCreateWatcher } from "../helpers";
-import { handleCreateLinear } from "./actions";
 
 function createContractLinear(address: Address): void {
   ContractLinear.create(address);
@@ -18,10 +17,11 @@ function createContractPro(address: Address): void {
 /**
  * Use the oldest linear contract as a trigger to start indexing all the other contracts.
  *
- * @param {CreateLinearStream} event
+ * @param {CreateLinearStream} _event
  * @returns
  */
-export function handleInitializer(event: CreateLinearStream): void {
+
+export function handleInitializer(_event: CreateLinearStream): void {
   let watcher = getOrCreateWatcher();
   if (watcher.isInitialized) {
     return;
@@ -45,13 +45,4 @@ export function handleInitializer(event: CreateLinearStream): void {
       createContractPro(Address.fromString(pros[i]));
     }
   }
-
-  /**
-   * ---------------
-   * By registering the contract on the 1st create event,
-   * we need to manually handle the registration of that one stream
-   * ---------------
-   * */
-
-  handleCreateLinear(event);
 }
