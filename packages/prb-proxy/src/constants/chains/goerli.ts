f8061b8570e52d226ec48623b58bfab18a93ad8d
Razvan Gabriel <razvan.gabriel.apostu@gmail.com>
2023-05-31 21:26:46 +0300

    feat(prb-proxy): update addresses and contract ABIs

--- packages/prb-proxy/src/constants/chains/goerli.ts
+++ packages/prb-proxy/src/constants/chains/goerli.ts
@@ -1,5 +1,5 @@
 export let chainId = 5;
 export let chain = "goerli";
-export let startBlock = 8621100;
+export let startBlock = 9098300;
 
-export let registry = "0x8afe5fe3bafa1fbc941a50b630aa966f3a7815a0";
+export let registry = "0xa87bc4c1bc54e1c1b28d2dd942a094a6b665b8c9";
