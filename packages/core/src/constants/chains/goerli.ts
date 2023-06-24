154c3745b4b16e1ef3e6833b6e83b3b796292f68
Razvan Gabriel <razvan.gabriel.apostu@gmail.com>
2023-06-24 18:55:07 +0300

    refactor: move loans management in optimism for testing

--- packages/core/src/constants/chains/goerli.ts
+++ packages/core/src/constants/chains/goerli.ts
@@ -9,12 +9,10 @@ export let startBlock = 9098200;
 
 export let linear: string[][] = [
   ["0xa2215702f518d425cc9f593be8a54efc3e4a08e9", "linear"],
-  ["0xae85837072cb833b46a0997daf1ff134341c340b", "linear-flash"],
 ];
 
 export let dynamic: string[][] = [
   ["0xb6e272431f555951844e0d071fb18e48e315132f", "dynamic"],
-  ["0xf63f3cf5b8523efd5950ecca9ef3f62cb0ee3476", "dynamic-flash"],
 ];
 /**
  * Pre-register some comptrollers to make sure fees and flash assets are indexed from the start,
@@ -22,7 +20,6 @@ export let dynamic: string[][] = [
  */
 export let comptroller: string[][] = [
   ["0xada5d13db8b414804357d2fc0b072373385cf308", "comptroller"],
-  ["0x18e78f3b6b835b09d5d20d70ac7afb1c8da6a034", "comptroller-flash"],
 ];
 
 /**
