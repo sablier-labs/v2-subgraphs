0b9d08bd98379e9c71593e00f043897d4f6e4096
Razvan Gabriel <razvan.gabriel.apostu@gmail.com>
2023-06-29 19:09:09 +0300

    chore(core): remove flash loan contracts

--- packages/core/src/constants/chains/arbitrum.ts
+++ packages/core/src/constants/chains/arbitrum.ts
@@ -9,12 +9,10 @@ export let startBlock = 9098200;
 
 export let linear: string[][] = [
   ["0xa2215702f518d425cc9f593be8a54efc3e4a08e9", "LL"],
-  ["0xae85837072cb833b46a0997daf1ff134341c340b", "LLF"],
 ];
 
 export let dynamic: string[][] = [
   ["0xb6e272431f555951844e0d071fb18e48e315132f", "LD"],
-  ["0xf63f3cf5b8523efd5950ecca9ef3f62cb0ee3476", "LDF"],
 ];
 
 /** PRBProxy registry */
