8302d10210a924868a1ea9423c0429c4899a78e8
Razvan Gabriel <razvan.gabriel.apostu@gmail.com>
2024-01-16 16:50:45 +0200

    chore: cleanup

--- apps/protocol/src/constants/index.ts
+++ apps/protocol/src/constants/index.ts
@@ -12,9 +12,6 @@ export let one = BigInt.fromI32(1);
 export let two = BigInt.fromI32(2);
 export let d18 = BigInt.fromI32(18);
 
-export let put = 0;
-export let call = 1;
-
 export let ADDRESS_ZERO = Bytes.fromHexString(
   "0x0000000000000000000000000000000000000000",
 );
