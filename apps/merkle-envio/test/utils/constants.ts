80249aff0f1d59b8a56f820a15d27ad5c65d5a02
Razvan Gabriel <razvan.gabriel.apostu@gmail.com>
2024-02-01 16:28:52 +0200

    chore: update envio endpoints

--- apps/merkle-envio/test/utils/constants.ts
+++ apps/merkle-envio/test/utils/constants.ts
@@ -1,7 +1,7 @@
 export type Vendor = "Envio" | "TheGraph";
 
 export const endpoint: Record<Vendor, string> = {
-  Envio: "https://indexer.bigdevenergy.link/89e6765/v1/graphql", // "http://localhost:8080/v1/graphql",
+  Envio: "https://indexer.bigdevenergy.link/61cf6be/v1/graphql", // "http://localhost:8080/v1/graphql",
   TheGraph:
     "https://api.thegraph.com/subgraphs/name/sablier-labs/sablier-v2-ms-experimental",
 };
--- apps/protocol-envio/test/utils/constants.ts
+++ apps/protocol-envio/test/utils/constants.ts
@@ -1,7 +1,7 @@
 export type Vendor = "Envio" | "TheGraph";
 
 export const endpoint: Record<Vendor, string> = {
-  Envio: "http://localhost:8080/v1/graphql",
+  Envio: "https://indexer.bigdevenergy.link/be2052e/v1/graphql",
   TheGraph:
     "https://api.thegraph.com/subgraphs/name/sablier-labs/sablier-v2-experimental",
 };
