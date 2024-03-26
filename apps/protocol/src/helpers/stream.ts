c9612a282ce8bcf28042db0224ed7e4222dd5266
Razvan Gabriel <razvan.gabriel.apostu@gmail.com>
2024-03-26 13:14:08 +0000

    feat(protocol): add tranche and lockup tranched stream entities
    feat(protocol): apply gateway migration for v2.2
    docs(protocol): add informal changelog to the gateway file

--- apps/protocol/src/helpers/stream.ts
+++ apps/protocol/src/helpers/stream.ts
@@ -2,12 +2,14 @@ import { BigInt, dataSource, ethereum, log } from "@graphprotocol/graph-ts";
 import { Stream } from "../generated/types/schema";
 import { CreateLockupDynamicStream as EventCreateDynamic } from "../generated/types/templates/ContractLockupDynamic/SablierV2LockupDynamic";
 import { CreateLockupLinearStream as EventCreateLinear } from "../generated/types/templates/ContractLockupLinear/SablierV2LockupLinear";
+import { CreateLockupTranchedStream as EventCreateTranched } from "../generated/types/templates/ContractLockupLinear/SablierV2LockupTranched";
 import { getChainId, one, zero } from "../constants";
 import { getOrCreateAsset } from "./asset";
 import { getOrCreateBatch } from "./batch";
 import { getContractByAddress } from "./contract";
 import { bindProxyOwner } from "./proxy";
 import { createSegments } from "./segments";
+import { createTranches } from "./tranches";
 import { getOrCreateWatcher } from "./watcher";
 
 function createStream(tokenId: BigInt, event: ethereum.Event): Stream | null {
@@ -156,6 +158,57 @@ export function createDynamicStream(event: EventCreateDynamic): Stream | null {
 
   /** --------------- */
   entity = createSegments(entity, event);
+
+  /** --------------- */
+  let resolved = bindProxyOwner(entity);
+
+  resolved.save();
+  return resolved;
+}
+
+export function createTranchedStream(
+  event: EventCreateTranched,
+): Stream | null {
+  let tokenId = event.params.streamId;
+  let entity = createStream(tokenId, event);
+
+  if (entity == null) {
+    return null;
+  }
+
+  /** --------------- */
+  entity.category = "LockupTranched";
+  entity.funder = event.params.funder;
+  entity.sender = event.params.sender;
+  entity.recipient = event.params.recipient;
+  entity.parties = [event.params.sender, event.params.recipient];
+
+  entity.depositAmount = event.params.amounts.deposit;
+  entity.brokerFeeAmount = event.params.amounts.brokerFee;
+  entity.protocolFeeAmount = zero;
+  entity.intactAmount = event.params.amounts.deposit;
+
+  entity.startTime = event.params.range.start;
+  entity.endTime = event.params.range.end;
+  entity.cancelable = event.params.cancelable;
+  entity.cliff = false;
+
+  entity.duration = event.params.range.end.minus(event.params.range.start);
+
+  /** --------------- */
+  let asset = getOrCreateAsset(event.params.asset);
+  entity.asset = asset.id;
+
+  /** --------------- */
+  let batch = getOrCreateBatch(event, event.params.sender);
+  entity.batch = batch.id;
+  entity.position = batch.size.minus(one);
+
+  /** --------------- */
+  entity.save();
+
+  /** --------------- */
+  entity = createTranches(entity, event);
 
   /** --------------- */
   let resolved = bindProxyOwner(entity);
