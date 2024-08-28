478c5063f2848c5abc709e323d6af1932c428d4c
Razvan Gabriel <razvan.gabriel.apostu@gmail.com>
2024-08-28 11:40:57 +0300

    fix(constants): remove iotex from bundles, keep in subgraphs

--- packages/constants/src/bundles/merkle-envio.ts
+++ packages/constants/src/bundles/merkle-envio.ts
@@ -4,7 +4,6 @@ import * as base from "../addresses/base";
 import * as blast from "../addresses/blast";
 import * as bsc from "../addresses/bsc";
 import * as gnosis from "../addresses/gnosis";
-import * as iotex from "../addresses/iotex";
 import * as linea from "../addresses/linea";
 import * as mainnet from "../addresses/mainnet";
 import * as optimism from "../addresses/optimism";
@@ -37,7 +36,6 @@ export const chains = () => {
     blast,
     bsc,
     gnosis,
-    iotex,
     linea,
     mainnet,
     optimism,
