a0ff250724b6a19f25bbd6c1917b2cb6a44aae19
Razvan Gabriel <razvan.gabriel.apostu@gmail.com>
2024-10-28 23:18:52 +0200

    chore: bump to build

--- apps/lockup-envio/src/_generated/original/bundles/lockup-envio.ts
+++ apps/lockup-envio/src/_generated/original/bundles/lockup-envio.ts
@@ -12,7 +12,7 @@ import * as optimism from "../addresses/optimism";
 import * as polygon from "../addresses/polygon";
 import * as scroll from "../addresses/scroll";
 import * as sepolia from "../addresses/sepolia";
-import * as tangle from "../addresses/tangle";
+// import * as tangle from "../addresses/tangle";
 import * as zksync from "../addresses/zksync";
 
 const available = (v: {
