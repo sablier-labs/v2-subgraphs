2688b17ce75e7206f39029c5957ed70fb5432e27
Mircea Gavriliu <gavriliumircea@yahoo.ro>
2024-10-31 21:25:53 +0200

    chore: rebase main

--- packages/constants/src/bundles/flow-envio.ts
+++ packages/constants/src/bundles/flow-envio.ts
@@ -1,4 +1,5 @@
 import * as experimental from "../addresses/experimental";
+import definitions from "./definitions";
 
 const available = (v: { flow: unknown[] }) => {
   return v.flow.length > 0;
@@ -16,12 +17,19 @@ const filter = (list: string[][], version: string) => {
   );
 };
 
+/**
+ * Bind a viem chain definition to a sablier indexer configuration.
+ * ↪ 🚨 Chains without valid viem definitions will not be taken into account.
+ */
+
 export const chains = () => {
-  const list = [experimental] as const;
+  const list = [
+    [experimental, definitions.sepolia],
+  ] as const;
 
-  /** Merging the linear and dynamic arrays with a spread operator will break mustache's template engine */
+  /** Merging the arrays with a spread operator will break mustache's template engine */
 
-  return list.map((item) => {
+  return list.map(([item, definition]) => {
     const V10 = {
       flow: filter(item.flow, "V10"),
       available: false,
@@ -30,10 +38,11 @@ export const chains = () => {
     V10.available = available(V10);
 
     return {
+      definition,
       id: item.chainId,
       name: item.chain,
       start_block: item.startBlock_merkle,
       V10,
     };
-  });
+  }).filter((item) => item.definition);
 };
