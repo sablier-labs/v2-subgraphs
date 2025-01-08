178a111e6b8c3d47e7d4e81e99b214230214511d
Razvan Gabriel <razvan.gabriel.apostu@gmail.com>
2025-01-08 15:49:10 +0200

    fix(merkle): handler import

--- apps/merkle/src/mappings/index.ts
+++ apps/merkle/src/mappings/index.ts
@@ -3,6 +3,9 @@ export {
   handleCreateCampaignLL_V21,
   handleCreateCampaignLL_V22,
   handleCreateCampaignLT_V22,
+  handleCreateCampaignLL_V23,
+  handleCreateCampaignLT_V23,
+  handleCreateCampaignInstant_V23,
 } from "./handle-factory";
 export {
   handleClaimInstant,
